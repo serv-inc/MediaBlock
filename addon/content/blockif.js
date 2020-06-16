bbf49d77b94005a8bba38ee73907bb0c807c537b
serv-inc <serv-inc@users.noreply.github.com>
2020-06-17 01:05:38 +0200

    code had more lint

--- addon/content/blockif.js
+++ addon/content/blockif.js
@@ -1,11 +1,9 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
 /* globals chrome */
-/* globals document */
-/* globals window */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
-const WHITELIST = RegExp("google.*newtab|google.*source[^\/]*url");
+const WHITELIST = RegExp("google.*newtab|google.*source[^/]*url");
 
 class Whitelist {
   

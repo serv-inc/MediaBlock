a0a2868aca49989fad60ecb2a9cea14c688932b3
serv-inc <serv-inc@users.noreply.github.com>
2020-06-17 23:57:31 +0200

    test had old path, was less pretty

--- test/whitelist.test.js
+++ test/whitelist.test.js
@@ -1,20 +1,19 @@
-const assert = require('assert').strict;
-var Whitelist = require('../addon/content/whitelist.js').default;
-var mockStorage = require('../meta/mockExt.js').default.storage;
+const assert = require("assert").strict;
+var Whitelist = require("../src/content/whitelist.js").default;
+var mockStorage = require("../meta/mockExt.js").default.storage;
 
-describe('Whitelist', function () {
-  it('should run', function () {
+describe("Whitelist", function () {
+  it("should run", function () {
     new Whitelist(mockStorage);
   });
-  it('should contain mock values', async function () {
-    let w = new Whitelist(storage=mockStorage);
-    let q = await w.get()
-    assert.match('hello', q);
+  it("should contain mock values", async function () {
+    let w = new Whitelist((storage = mockStorage));
+    let q = await w.get();
+    assert.match("hello", q);
   });
-  it('should be initialized', async function () {
-    let w = new Whitelist(storage=mockStorage);
-    let q = await w.get()
+  it("should be initialized", async function () {
+    let w = new Whitelist((storage = mockStorage));
+    let q = await w.get();
     assert.ok(w.initialized);
   });
 });
-

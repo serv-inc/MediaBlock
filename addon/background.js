fe2115d9adafb88f529110f2713ea77fc32960bc
serv-inc <serv-inc@users.noreply.github.com>
2020-06-18 23:36:00 +0200

    lack: background failed to query whitelist

--- addon/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(isOk);
-
-function isOk(request, sender, sendResponse) {
-  const u = new URL(sender.url);
-  sendResponse({ task: "isOk", data: { name: u.host, isOk: true } });
-}
---
+++ src/browserify/background.js
@@ -0,0 +1,27 @@
+"use strict";
+/* globals chrome */
+// licensed under the MPL 2.0 by (github.com/serv-inc)
+
+/** fileoverview send whitelist when messaged */
+
+const Whitelist = require("./whitelist.js").default;
+const whitelist = new Whitelist();
+
+chrome.runtime.onMessage.addListener(isOk);
+function isOk(request, sender, sendResponse) {
+  chrome.tabs.query({ active: true }, (tabs) => {
+    const activeTab = tabs[0];
+    // todo: fails on page load, leaves popup empty, error in log
+    const u = new URL(activeTab.url);
+    whitelist
+      .get()
+      .then((wreg) =>
+        sendResponse({
+          task: "isOk",
+          data: { name: u.host, isOk: wreg.test(u.host) },
+        })
+      )
+      .catch((err) => console.log("error is " + err));
+  });
+  return true; // keep channel open
+}

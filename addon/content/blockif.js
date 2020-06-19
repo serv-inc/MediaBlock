5b18064aed91edb96fbcebe2dff1b0e931f3acf4
serv-inc <serv-inc@users.noreply.github.com>
2020-06-19 11:29:07 +0200

    content script did not use background whitelist

---
+++ addon/content/blockif.js
@@ -0,0 +1,49 @@
+"use strict";
+/* globals chrome */
+// licensed under the MPL 2.0 by (github.com/serv-inc)
+
+/**
+ * @fileoverview hide images if page does not match whitelist
+ */
+const HIDE_STYLE = `img, video {
+    display: none !important; /* 1 */
+    height: 1px; /* 3 */
+    overflow: hidden; /* 3 */
+    visibility: hidden !important; /* 2 */
+    width: 1px; /* 3 */
+}`;
+const HIDE_STYLE_ID = "good-company-hide-media";
+
+function hideIf(ok) {
+  let style = document.querySelector("style#" + HIDE_STYLE_ID);
+  if (!ok && style === null) {
+    style = document.createElement("style");
+    style.id = HIDE_STYLE_ID;
+    document.head.appendChild(style);
+    style.innerHTML = HIDE_STYLE;
+  } else if (ok && !!style) {
+    document.head.removeChild(style);
+  }
+}
+
+function getLocation() {
+  if (window.parent === window) {
+    // top frame
+    return document.location;
+  } else {
+    // iframe
+    return document.referrer;
+  }
+}
+
+async function main() {
+  chrome.runtime.sendMessage(
+    { task: "isOkWithUrl", url: getLocation() },
+    function handler(response) {
+      hideIf(response.data.isOk);
+      //    chrome.browserAction.setIcon({path: "a.png"});  // only from bg, overhead
+    }
+  );
+}
+
+main();
--- src/browserify/background.js
+++ src/browserify/background.js
@@ -7,7 +7,31 @@
 const Whitelist = require("./whitelist.js").default;
 const whitelist = new Whitelist();
 
-chrome.runtime.onMessage.addListener(isOk);
+chrome.runtime.onMessage.addListener(route);
+/** routes messages */
+function route(request, sender, sendResponse) {
+  if (request.task === "isOkWithUrl" && !!request.url) {
+    return isOkWithUrl(request, sender, sendResponse);
+  } else if (request.task === "isOk" && !request.url) {
+    return isOk(request, sender, sendResponse);
+  } else {
+    console.log("unknown: " + request);
+  }
+}
+
+/** is active / passed url good company ? */
+function isOkWithUrl(request, sender, sendResponse) {
+  console.log("isOkWithUrl");
+  console.log(request.url);
+  whitelist.get().then((wreg) =>
+    sendResponse({
+      task: "isOkWithUrl",
+      data: { isOk: wreg.test(request.url.host) },
+    })
+  );
+  return true;
+}
+
 /** is active / passed url good company ? */
 function isOk(request, sender, sendResponse) {
   chrome.tabs.query({ active: true }, (tabs) => {
@@ -26,3 +50,14 @@ function isOk(request, sender, sendResponse) {
   });
   return true; // keep channel open
 }
+/*
+function getWhitelist(request, sender, sendResponse) {
+    whitelist
+      .get()
+      .then((wreg) =>
+        sendResponse({
+          task: "isOk",
+          data: { name: u.host, isOk: wreg.test(u.host) },
+        })
+      )
+  */

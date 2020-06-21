15b565be9d16103bc44b9dab0597ea4ca210e50f
serv-inc <serv-inc@users.noreply.github.com>
2020-06-21 12:38:11 +0200

    was uglier: did not use components at all

--- src/react/popup.jsx
+++ src/react/popup.jsx
@@ -2,32 +2,38 @@
 /* globals chrome */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 /** @fileinfo: show browser action popup */
+function Anchor(prop) {
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={"https://" + prop.domain}
+    >
+      {prop.domain}
+    </a>
+  );
+}
+
+function Domain(prop) {
+  return (
+    <li id={prop.domain} key={prop.domain}>
+      <Anchor domain={prop.domain} />
+    </li>
+  );
+}
+
 function status(domain) {
   return domain.name + " is " + (domain.isOk ? "good " : "bad ") + " company";
 }
 
 function formatWhitelistManaged(whitelist) {
-  return <ul>{whitelist.data.managed.map((d) => formatDomain(d))}</ul>;
-}
-
-function formatDomain(domain) {
   return (
-    <li id={domain} key={domain}>
-      {toAnchor(domain)}
-    </li>
+    <ul>
+      {whitelist.data.managed.map((d) => (
+        <Domain key={d} domain={d} />
+      ))}
+    </ul>
   );
-}
-
-function toAnchor(domain) {
-  return (
-    <a target="_blank" rel="noopener noreferrer" href={toUrl(domain)}>
-      {domain}
-    </a>
-  );
-}
-
-function toUrl(domain) {
-  return "https://" + domain;
 }
 
 chrome.runtime.sendMessage({ task: "isOk" }, function handler(response) {

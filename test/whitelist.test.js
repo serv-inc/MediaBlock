8c8085db3406c432395d7b2f28b9abcf9a254bbe
serv-inc <serv-inc@users.noreply.github.com>
2020-06-22 10:24:35 +0200

    ugly(test): lacked global declaration
    - had named argument, which prettier formatted weirdly

--- test/whitelist.test.js
+++ test/whitelist.test.js
@@ -1,3 +1,4 @@
+/* globals describe, it */
 const assert = require("chai").assert;
 const Whitelist = require("../src/browserify/whitelist.js").default;
 const mockStorage = require("../meta/mockExt.js").default.storage;
@@ -8,32 +9,32 @@ describe("Whitelist", function () {
   });
 
   it("should be initialized", async function () {
-    let w = new Whitelist((storage = mockStorage));
+    let w = new Whitelist(mockStorage);
     let q = await w.get();
     assert.ok(w.initialized);
   });
 
   it("should contain mock values", async function () {
-    let w = new Whitelist((storage = mockStorage));
+    let w = new Whitelist(mockStorage);
     let q = await w.get();
     assert.match("hello", q);
   });
 
   it("should not contain other values", async function () {
-    let w = new Whitelist((storage = mockStorage));
+    let w = new Whitelist(mockStorage);
     let q = await w.get();
     assert.notMatch("not existing", q);
   });
 
   it("should return the same values", async function () {
-    let w = new Whitelist((storage = mockStorage));
+    let w = new Whitelist(mockStorage);
     let q = await w.get();
     let q2 = await w.get();
     assert.deepEqual(q2, q);
   });
 
   it("should allow adding", async function () {
-    let w = new Whitelist((storage = mockStorage));
+    let w = new Whitelist(mockStorage);
     await w.add("asdf");
     let q = await w.get();
     assert.match("asdf", q);

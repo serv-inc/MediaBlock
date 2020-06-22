c92e11df1aa6fd9e6b1c02e3d0dbcbbe06667279
serv-inc <serv-inc@users.noreply.github.com>
2020-06-22 11:28:08 +0200

    failed(popup): if page was loading

--- src/browserify/background.js
+++ src/browserify/background.js
@@ -19,13 +19,41 @@ function route(request, sender, sendResponse) {
     return isOkWithUrl(request, sender, sendResponse);
   } else if (request.task === "isOk" && !request.url) {
     return isOk(request, sender, sendResponse);
-  } else if (request.task === "getWhitelist") {
-    return getWhitelist(request, sender, sendResponse);
   } else if (request.task === "addToWhitelist" && request.addthis) {
     return addToWhitelist(request, sender, sendResponse);
+  } else if (request.task === "getWhitelist") {
+    return getWhitelist(request, sender, sendResponse);
   } else {
     console.log("unknown: " + request);
   }
+}
+
+/** is active / passed url good company ? */
+function isOkWithUrl(request, sender, sendResponse) {
+  whitelist.get().then((wreg) =>
+    sendResponse({
+      task: "isOkWithUrl",
+      data: { isOk: wreg.test(request.url.host), name: request.url.host },
+    })
+  );
+  return true; // keep channel open
+}
+
+/** is active / passed url good company ? */
+function isOk(request, sender, sendResponse) {
+  chrome.tabs.query({ active: true }, (tabs) => {
+    const activeTab = tabs[0];
+    console.log(activeTab.url);
+    if (typeof activeTab.url === "undefined" || activeTab.url.length == 0) {
+      // todo: replace with tabs.onActivated or sth if that is used (also for icon)
+      request.url = { host: "page loading, unknown domain" };
+    } else {
+      const u = new URL(activeTab.url);
+      request.url = u;
+    }
+    isOkWithUrl(request, sender, sendResponse);
+  });
+  return true; // keep channel open
 }
 
 function addToWhitelist(request, sender, sendResponse) {
@@ -45,35 +73,5 @@ function getWhitelist(request, sender, sendResponse) {
       data: wreg,
     })
   );
-  return true; // keep channel open
-}
-
-/** is active / passed url good company ? */
-function isOkWithUrl(request, sender, sendResponse) {
-  whitelist.get().then((wreg) =>
-    sendResponse({
-      task: "isOkWithUrl",
-      data: { isOk: wreg.test(request.url.host) },
-    })
-  );
-  return true; // keep channel open
-}
-
-/** is active / passed url good company ? */
-function isOk(request, sender, sendResponse) {
-  chrome.tabs.query({ active: true }, (tabs) => {
-    const activeTab = tabs[0];
-    // todo: fails while page is loading, leaves popup empty, error in log
-    const u = new URL(activeTab.url);
-    whitelist
-      .get()
-      .then((wreg) => {
-        sendResponse({
-          task: "isOk",
-          data: { name: u.host, isOk: wreg.test(u.host) },
-        });
-      })
-      .catch((err) => console.log("error is " + err));
-  });
   return true; // keep channel open
 }

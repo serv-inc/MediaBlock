a8659a813e3e82decb58895108508962d1641981
serv-inc <serv-inc@users.noreply.github.com>
2020-06-24 16:18:24 +0200

    lacked(popup): switch lacked status

--- src/react/Switch.js
+++ src/react/Switch.js
@@ -2,18 +2,20 @@
 
 based upon https://upmostly.com/tutorials/build-a-react-switch-toggle-component
 */
-const Switch = () => {
+const Switch = ({ isOn, handleToggle, label }) => {
   return (
-    <div>
+    <span>
       <input
+        checked={isOn}
+        onChange={handleToggle}
         className="react-switch-checkbox"
         id={`react-switch-new`}
         type="checkbox"
       />
       <label className="react-switch-label" htmlFor={`react-switch-new`}>
-        <span className={`react-switch-button`} />
+        <span className={`react-switch-button`}>{label}</span>
       </label>
-    </div>
+    </span>
   );
 };
 

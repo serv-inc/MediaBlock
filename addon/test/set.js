dc8ac3fcbf9030577bfb5ce56e23a2b28ecde8c4
serv-inc <serv-inc@users.noreply.github.com>
2020-09-18 23:53:18 +0200

    lacked set loading

--- addon/test/set.js
+++ addon/test/set.js
@@ -1,3 +1,54 @@
-const $set = {
-  initialized: true,
-};
+/* globals chrome */
+class Set {
+  constructor() {
+    this.loadedLocal = false;
+    this.storage = {};
+  }
+
+  get loaded() {
+    return this.loadedLocal;
+  }
+
+  async load() {
+    if (!this.loadedLocal) {
+      return await this._loadLocal();
+    }
+    return true;
+  }
+
+  async save() {
+    // could be quicker to save single
+    chrome.storage.local.set(this.storage);
+  }
+
+  async _loadLocal() {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(null, (result) => {
+        if (typeof result !== "undefined") {
+          for (let el in result) {
+            if (Object.prototype.hasOwnProperty.call(result, el)) {
+              this._addToSettings(el, result[el]);
+            }
+          }
+        }
+        this.loadedLocal = true;
+        resolve();
+      });
+    });
+  }
+}
+
+const handler = {
+  set: (obj, prop, value) => {
+    obj.storage[prop] = value;
+    obj.save(prop);
+  },
+  get: (obj, prop) => {
+    return obj.storage[prop];
+  },
+  apply: (target, thisArg, args) => {
+    return target(args);
+  },
+}; //new Proxy(new Set(), handler);
+
+const $et = new Set();
--- addon/test/tests.spec.js
+++ addon/test/tests.spec.js
@@ -1,15 +1,38 @@
+/* globals $et, describe, it, chai */
 const expect = chai.expect;
 
-describe("code", () => {
-  it("might work", () => {
+describe("testing the tests", () => {
+  it("should work", () => {
     expect(1 + 1).to.equal(2);
   });
 });
 
 describe("settings", () => {
+  it("exists", () => {
+    expect($et).to.be.ok;
+  });
+
   it("loads", async () => {
-    expect($set).to.be.ok;
-    await $set.load();
-    expect($set.initialized).to.be(true);
+    await $et.load();
+    expect($et.loaded).to.equal(true);
+  });
+
+  describe("storage", () => {
+    beforeEach(async () => {
+      $et.load();
+    });
+
+    it("saves", async () => {
+      $et.hello = "world";
+      await $et.load();
+      expect($et.hello).to.equal("world");
+    });
+
+    it("saves and next has it", async () => {
+      $et.hello = "world";
+      const $2 = new Set();
+      await $2.load();
+      expect($2.hello).to.equal("world");
+    });
   });
 });
